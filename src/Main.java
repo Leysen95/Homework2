5b2de57ec3c83e4719538ca3e7b3783f9a6723d8
Leysen95 <mullayanova95@bk.ru>
2024-05-03 18:25:53 +0500

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,12 @@ public class Main {
         cat = cat + 4;
         System.out.println(cat);
         paper = paper + 4;
-        System.out.println(paper);}
+        System.out.println(paper);
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+    }
 }
